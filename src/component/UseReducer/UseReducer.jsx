01161b6b8742bd8c0674ebc6150016844c17d529
Sonu Mishra <155150337+ItsmeSonu23@users.noreply.github.com>
2025-07-28 16:18:59 +0530

    UseReducer: Implemented useReducer hook for state management

--- src/component/UseReducer/UseReducer.jsx
+++ src/component/UseReducer/UseReducer.jsx
@@ -4,39 +4,39 @@ const initialState = { count: 0 }
 const initialMode = { mode: 'light' }
 
 const reducer = (state, action) => {
-  switch (action.type) {
-    case 'increment':
-      return { count: state.count + 1 }
-    case 'decrement':
-      return { count: state.count - 1 }
-    default:
-      return state
-  }
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + 1 }
+        case 'decrement':
+            return { count: state.count - 1 }
+        default:
+            return state
+    }
 }
 
 const modeReducer = (state, action) => {
     switch (action.type) {
-      case 'dark':
-        return { ...state, mode: 'dark' }
-      case 'light':
-        return { ...state, mode: 'light' }
-      default:
-        return state
+        case 'dark':
+            return { ...state, mode: 'dark' }
+        case 'light':
+            return { ...state, mode: 'light' }
+        default:
+            return state
     }
-  }
+}
 
 const UseReducer = () => {
-  const [state, dispatch] = useReducer(reducer, initialState)
-  const [mode, dispatchMode] = useReducer(modeReducer, initialMode)
-  return (
-    <div className={`h-screen w-full flex flex-col items-center justify-center ${mode.mode==="dark"? 'bg-gray-800 text-white' : 'bg-white text-black'}`}>
-      <h1>Count: {state.count}</h1>
-      <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
-      <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+    const [state, dispatch] = useReducer(reducer, initialState)
+    const [mode, dispatchMode] = useReducer(modeReducer, initialMode)
+    return (
+        <div className={`h-screen w-full flex flex-col items-center justify-center ${mode.mode === "dark" ? 'bg-gray-800 text-white' : 'bg-white text-black'}`}>
+            <h1>Count: {state.count}</h1>
+            <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+            <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
 
-      <button onClick={() => dispatchMode({ type: mode.mode === 'light' ? 'dark' : 'light' })}>Mode</button>
-    </div>
-  )
+            <button onClick={() => dispatchMode({ type: mode.mode === 'light' ? 'dark' : 'light' })}>Mode</button>
+        </div>
+    )
 }
 
 export default UseReducer;
