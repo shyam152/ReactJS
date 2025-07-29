1d2744d1c78342000b5e8e9f7e1436edd90a1022
Sonu Mishra <155150337+ItsmeSonu23@users.noreply.github.com>
2025-07-29 16:34:25 +0530

    Router

---
+++ src/routercomp/UI/Footer.jsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+  return (
+    <div>
+      Footer Component
+      <p>© 2023 Your Company Name. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/routercomp/UI/Outlet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Outlet = () => {
+  return (
+    <div>
+      Outlet Component
+    </div>
+  )
+}
+
+export default Outlet
