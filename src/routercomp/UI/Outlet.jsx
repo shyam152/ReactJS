887fa2e9a944d44b8e9424ae508bd149493c0f9c
Sonu Mishra <155150337+ItsmeSonu23@users.noreply.github.com>
2025-07-30 11:10:54 +0530

    New Outlet

--- src/routercomp/UI/Outlet.jsx
+++ src/routercomp/UI/Outlet.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import { Outlet } from 'react-router-dom'
+
+/**
+ * Outlet component that renders the Header, Outlet (from React Router), and Footer components.
+ *
+ * @returns {React.ReactElement} The rendered components.
+ */
 
 const Outlet = () => {
-  return (
-    <div>
-      Outlet Component
-    </div>
-  )
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />    
+        </>
+    )
 }
 
 export default Outlet
