df3a3b23205579cb2604de63ebc72b95997f64c8
shyam152 <shyamjangid453@gmail.com>
2025-08-02 11:28:43 +0530

    Update Card.jsx

--- src/component/Styling/Card.jsx
+++ src/component/Styling/Card.jsx
@@ -18,12 +18,20 @@ const Card = ({details}) => {
     <div className={`h-96 w-[300px] items-stretch text-gray-600 shadow-2xl p-5 ${rating>8.7 ? "bg-violet-900":"bg-red-300"}`}>
       <h2 className='m-0 text-2xl'>Card Title {title}</h2>
       <p className='font-bold text-white'>{desc}</p>
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
       <p className='font-bold'>Rating <span >{rating}</span> </p>
       <p className='font-bold'>Casts : {
         casts.map((e)=><span>{e}, </span>)
         }
       </p>
       <button className='bg-violet-400 px-5 py-2 w-full rounded-2xl'>Watch Now</button>
+=======
+      <button>Watch Now</button>
+>>>>>>> Stashed changes
+=======
+      <button>Watch Now</button>
+>>>>>>> Stashed changes
     </div>
   )
 }
